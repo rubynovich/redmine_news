b721c6391e4e68ca68bd8778be67daf52cc2504c
rubynovich <roman@shipiev.me>
2013-02-15 10:13:47 +0400

    fix

--- init.rb
+++ init.rb
@@ -5,6 +5,6 @@ Redmine::Plugin.register :redmine_news do
   author 'Roman Shipiev'
   description 'Fix News-module (views for index and show)'
   version '0.0.1'
-  url 'https://github.com/rubynovich/redmine_news.git'
+  url 'https://github.com/rubynovich/redmine_news'
   author_url 'http://roman.shipiev.me'
 end
